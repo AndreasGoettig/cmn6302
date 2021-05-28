68e1a6e88b829438aaf5172d7a3c8df673702d03
AndreasG <Goettig.Andreas@gmx.de>
2021-05-28 16:04:21 +0200

    ui für quest hinzugefügt

---
+++ Assets/Andys Tests/QuestManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class QuestManager : MonoBehaviour
+{
+    public TMP_Text description;
+    public TMP_Text progress;
+    public Image doneCheck;
+
+    public Quest defaultQuest;
+    public Quest wheat;
+    public Quest battle;
+    public Quest deliver;
+    public Quest goTo;
+
+    private Quest currentQuest;
+    private bool QuestInProgress = false;
+
+    public void Update()
+    {
+        CheckCurrentStatus();
+        CheckCurrentQuestStatus();
+    }
+
+    public void CheckCurrentStatus()
+    {
+        if (!QuestInProgress)
+        {
+            LoadQuestData(defaultQuest);
+        }
+    }
+    public void CheckCurrentQuestStatus()
+    {
+        if (progress.text == currentQuest.questMax+"/"+currentQuest.questMax && progress.text != "0/0")
+        {
+            //Quest done
+        }
+    }
+    public void UpdateQuestStatus()
+    {
+
+    }
+
+    public void LoadQuestData(Quest data)
+    {
+        description.text = data.description;
+        progress.text = data.questMin + "/" + data.questMax;
+        if (progress.text=="0/0")
+        {
+            doneCheck.gameObject.SetActive(false);
+            progress.text = "";
+        }
+        currentQuest = defaultQuest;
+    }
+}
---
+++ Assets/Andys Tests/quests/Quest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "testquest", menuName = "ScriptableObjects/questData", order = 1)]
+public class Quest : ScriptableObject
+{
+    public string description;
+    public int questMax;
+    public int questMin;
+}
